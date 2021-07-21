2fb8be053d63623bc3b2da3795630ae0be9bae40
fernando millanao carril <fer.millanao@duocuc.cl>
2021-07-21 14:04:02 -0400

    a

--- examen/core/admin.py
+++ examen/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Arbustos, Categoria, Flores, Tierradehojas, maceteros
+from .models import Arbustos, Categoria, Flores, Registro, Tierradehojas, maceteros
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(Flores)
 admin.site.register(maceteros)
 admin.site.register(Arbustos)
 admin.site.register(Tierradehojas)
+admin.site.register(Registro)
